21ec12dbc9951c52741953346decba89038eac09
Hong Sung Jun <sjtheorange@gmail.com>
2021-07-26 14:57:18 +0000

    made basic greyscale + sobel conv

--- filter.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-
-int main(int argc, char *argv[]){
-    printf("%s\n",argv[0]);
-    printf("i'm out of here");
-    return 0;
-}
---
+++ grey.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
+int main(int argc, char *argv[]){
+    
+    
+    
+    int width, height, bpp;
+
+    uint8_t* rgb_image = stbi_load(argv[1], &width, &height, &bpp, 0);
+
+    
+    printf("width is :%d, height is :%d, bpp is %d\n",width,height,bpp);
+    uint8_t* new_image = malloc(sizeof(uint8_t) * (width*height));
+    uint8_t* p = rgb_image;
+    uint8_t* q = new_image;
+    for (p,q; p != rgb_image + (width*height*bpp); p += bpp, q += 1){
+        uint8_t red = *(p);
+        uint8_t green = *(p+1);
+        uint8_t blue = *(p+2);
+        
+        uint8_t grey = (red / 3) + (green / 3) + (blue/3);
+        *(q) = grey;
+    }
+    //const char* result_jpg = sprintf("%s_result.jpg",argv[1]);
+   // const char* result_png = sprintf("%s_result.png",argv[1]);
+    stbi_write_jpg(argv[2],width,height,1,(void*)new_image,100);
+    stbi_write_png(argv[3],width,height,1,(void*)new_image,width);
+    return 0;
+    
+
+}
